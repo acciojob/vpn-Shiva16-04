1db2a383096a7a830da03a4623daaab6327c4533
Shiva16-04 <shivakumaryedla1999@gmail.com>
2023-11-04 16:16:43 +0530

    changes related to boolean

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -24,13 +24,13 @@ public class User {
     public User() {
     }
 
-    public User(int id, String userName, String password, String originalIp, String maskedIp, boolean connected, List<ServiceProvider> serviceProviderList, List<Connection> connectionList, Country country) {
+    public User(int id, String userName, String password, String originalIp, String maskedIp, boolean connected1, List<ServiceProvider> serviceProviderList, List<Connection> connectionList, Country country) {
         this.id = id;
         this.userName = userName;
         this.password = password;
         this.originalIp = originalIp;
         this.maskedIp = maskedIp;
-        this.connected = connected;
+        connected = connected1;
         this.serviceProviderList = serviceProviderList;
         this.connectionList = connectionList;
         this.country = country;
@@ -80,8 +80,8 @@ public class User {
         return connected;
     }
 
-    public void setConnected(boolean connected) {
-        this.connected = connected;
+    public void setConnected(boolean connected1) {
+        connected = connected1;
     }
 
     public List<ServiceProvider> getServiceProviderList() {
